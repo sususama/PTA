ee34bd8dc616e8b69dd75bc8f4ab0b7790afa989
王旭昊 <1023605039@qq.com>
2020-05-16 14:35:38 +0800

    Previous code

---
+++ src/LeetCode/test/Main.java
@@ -0,0 +1,116 @@
+package LeetCode.test;
+
+import LeetCode.剑指offer.MaxSlidingWindow;
+
+public class Main {
+    public static void main(String[] args) {
+//        List
+//        ListIterator
+
+
+/* 945       int a [] = {1, 1, 3, 3, 5, 1};
+        NineHundredAndFortyFive n = new NineHundredAndFortyFive();
+        n.minIncrementForUnique(a);*/
+/* 566        FiveHundredAndSixtySix a = new FiveHundredAndSixtySix();
+        int [] [] nums = {{1,2,3,4}} ;
+        int r = 2, c = 2;
+        nums = a.matrixReshape(nums,r,c);
+        for (int i = 0 ;i<nums.length;i++)
+            for (int j = 0 ; j <nums.length; j++)
+                System.out.println(nums[i][j]);*/
+
+/*
+//121
+        int [] a = {1,2};
+        OneHundredAndTwentyOne b = new OneHundredAndTwentyOne();
+        b.maxProfit(a);
+*/
+//        List list = new ArrayList();
+//        Collections c = new Collections(list);
+//319
+//        ThreeHundredAndNineteen a = new ThreeHundredAndNineteen();
+//        System.out.println(a.bulbSwitch(4));
+//        System.out.println(Math.sqrt(3));
+//        // 504
+//        FiveHundredAndFour a = new FiveHundredAndFour();
+//        System.out.println(a.convertToBase7(100));
+        //557
+//        FiveHundredAndFiverSeven a = new FiveHundredAndFiverSeven();
+//        System.out.println(a.reverseWords("Let's take LeetCode contest"));
+        //1290
+//        System.out.println(5.0/2);
+        //面试题60
+//        TwoSum a = new TwoSum();
+//        a.twoSum(2);
+//        boolean a [] [] = new boolean[3][3];
+//        System.out.println(a[1][2]);
+//        List<Integer> sum = new ArrayList<>();
+//        System.out.println(sum.toArray());
+//        Queue <Integer> q;
+//        System.out.println(3%5);
+//        ThreeHundredTwentyTwo a = new ThreeHundredTwentyTwo();
+//        int b [] = {2};
+//        System.out.println(a.coinChange(b,0));
+//        test();
+//        String str = "as";
+//            String tempStr = "";
+//            try {
+//                tempStr = new String(str.getBytes("ISO-8859-1"), "GBK");
+//                tempStr = tempStr.trim();
+//            }
+//            catch (Exception e) {
+//                System.err.println(e.getMessage());
+//            }
+//            System.out.println(tempStr);
+
+//        ThreeHundredAndSixthFive a = new ThreeHundredAndSixthFive();
+//        System.out.println(a.canMeasureWater(34, 5, 6));
+//        EightHundredAndFiftyThree a = new EightHundredAndFiftyThree();
+//        int b [] = {6, 8};
+//        int c [] = {3, 2};
+//        System.out.println(a.carFleet(10, b, c));
+//        System.out.println(5/2);
+//        Massage a = new Massage();
+//        int [] b = {1,2,3,1};
+//        System.out.println(a.massage(b));
+//        NinetyHundredAndTenFour a = new NinetyHundredAndTenFour();
+//        int[] b = {1,1,1,2,2,2,3,3};
+//        System.out.println(a.hasGroupsSizeX(b));
+//        OneThousandAndOneHundredSixtyTwo a = new OneThousandAndOneHundredSixtyTwo();
+//        int [][] b = {{1,0,0},{0,0,0},{0,0,0}};
+//        System.out.println(a.maxDistance(b));
+//        int[][] a = new int[2][0];
+//        for (int i = 0; i < 2; i++) {
+//            a[i][0] = i;
+//            System.out.println(a[i][0]);
+//        }
+//        int[] a = {1,3,-1,-3,5,3,6,7};
+//        MaxSlidingWindow b = new MaxSlidingWindow();
+//        System.out.println(b.maxSlidingWindow(a,3));
+
+    }
+//    public static void test() {
+//        String s = "a";
+//        for(int i = 0; i < 100000; i++) {
+//            s += "a";
+//        }
+//        /****************toCharArray遍历*************/
+//        long start1 = System.currentTimeMillis();
+//        char[] arr = s.toCharArray();
+//        for (int i = 0; i < arr.length; i++) {
+//            char c = arr[i];
+//            System.out.println(c);
+//        }
+//        long end1 = System.currentTimeMillis();
+//        /****************charAt遍历******************/
+//        long start2 = System.currentTimeMillis();
+//        for(int i = 0; i < s.length(); i++) {
+//            char c = s.charAt(i);
+//            System.out.println(c);
+//        }
+//        long end2 = System.currentTimeMillis();
+//
+//        System.out.println(end1 - start1);  //503
+//        System.out.println(end2 - start2);  //453
+//    }
+}
