3ee82b37f1e985a37fa72c99ad56e308262d5011
王旭昊 <1023605039@qq.com>
2020-06-10 22:00:03 +0800

    Previous code

--- src/LeetCode/OneToFiveHundred/Nine.java
+++ src/LeetCode/OneToFiveHundred/Nine.java
@@ -1,4 +1,13 @@
 package LeetCode.OneToFiveHundred;
 
 public class Nine {
+    public boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        StringBuffer sb = new StringBuffer(str);
+        sb.reverse();
+        StringBuffer sb1 = new StringBuffer(str);
+        String s = sb.toString();
+        String s1 = sb1.toString();
+        return s.equals(s1);
+    }
 }
