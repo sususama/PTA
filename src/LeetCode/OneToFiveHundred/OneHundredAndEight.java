d5ec3219a4fc09d4cc47cd450218490f7622a10f
王旭昊 <1023605039@qq.com>
2020-07-03 23:26:00 +0800

    Today

--- src/LeetCode/OneToFiveHundred/OneHundredAndEight.java
+++ src/LeetCode/OneToFiveHundred/OneHundredAndEight.java
@@ -4,7 +4,16 @@ import LeetCode.TreeNode;
 
 public class OneHundredAndEight {
     public TreeNode sortedArrayToBST(int[] nums) {
-
-        return null;
+        // 平衡二叉树的中序遍历是从小到大的，我们可以反向构造平衡二叉树
+        return inverseMiddleOrderTraversal(nums ,0 , nums.length - 1);
+    }
+    private TreeNode inverseMiddleOrderTraversal(int nums[], int left, int right){
+        // 先进行中序遍历，将左子树总设为根节点
+        if (left > right) return null;
+        int mid = (left + right) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = inverseMiddleOrderTraversal(nums, left, mid - 1);
+        root.right = inverseMiddleOrderTraversal(nums, mid + 1, right);
+        return root;
     }
 }
